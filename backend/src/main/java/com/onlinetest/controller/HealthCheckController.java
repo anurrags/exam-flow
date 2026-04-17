80a77e5f914046e6a18003a1b4b42c380df13bf3
Anurag Sharma <iamanurag0501@gmail.com>
2026-04-17 15:43:43 +0530

    health check log

--- backend/src/main/java/com/onlinetest/controller/HealthCheckController.java
+++ backend/src/main/java/com/onlinetest/controller/HealthCheckController.java
@@ -4,13 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class HealthCheckController{
 
   @GetMapping("/health")
   public String healthCheck(){
-    return "OK";
+    log.info("Health check called");
+    return "OK";  
   }
 }
